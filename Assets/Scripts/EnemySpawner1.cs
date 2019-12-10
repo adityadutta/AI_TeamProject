5633fb22adbcdddea579a371c4434e5361a02f52
Mike Wells <mwcwells@gmaill.com>
2019-12-09 20:27:36 -0500

    -Added new enemy ship and ship spawning in the level

---
+++ Assets/Scripts/EnemySpawner1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner1 : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float SpawnerDelay;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > SpawnerDelay)
+        {
+            SpawnEnemyShip();
+            timer = 0.0f;
+        }
+    }
+
+    //Spawn enemy ship
+    void SpawnEnemyShip()
+    {
+        Vector3 SpawnPoint = new Vector3(transform.position.x + Random.Range(-7.5f, 7.5f), transform.position.y, transform.position.z);
+        GameObject tempEnemy = (GameObject)Instantiate(enemyPrefab, SpawnPoint, Quaternion.identity);
+        tempEnemy.transform.Rotate(0.0f, 0.0f, 180.0f);
+    }
+        
+}
---
+++ Assets/Scripts/MoveForwardShip.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForwardShip : MonoBehaviour {
+
+    public float maxYSpeed = 5.0f;
+    public float maxXSpeed;
+
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    public float timeRangemin;
+    public float timeRangeMax;
+    private float timeRange;
+    private float timer;
+
+    private void Start()
+    {
+        timeRange = Random.Range(timeRangemin, timeRangeMax);
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //Move ship forwards
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(maxXSpeed * Time.deltaTime, maxYSpeed * Time.deltaTime, 0);
+        pos += transform.rotation * velocity;
+        transform.position = pos;
+
+
+        Debug.Log("X Location: " + transform.position.x + " | Y Location: " + transform.position.y);
+
+        if(transform.position.y < yMin)
+        {
+            ResetYLocation();
+        }
+
+        //Check the location of the ship in the x location
+        if (xMin > transform.position.x || xMax < transform.position.x)
+        {
+            ResetXDirection();
+        }
+
+        //Update timer for location
+        timer += Time.deltaTime;
+
+        if (timer > timeRange)
+        {
+            ResetXDirection();
+        }
+
+    }
+
+    void ResetYLocation()
+    {
+        Vector3 newPos = new Vector3(transform.position.x, yMax, 0.0f);
+        transform.position = newPos;
+    }
+    
+
+    void ResetXDirection()
+    {
+        maxXSpeed *= -1.0f;
+        timer = 0.0f;
+        timeRange = Random.Range(timeRangemin, timeRangeMax);
+    }
+}
